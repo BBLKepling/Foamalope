d0b721fe5c38f9b0dd9fff243ee4bd5552385739
BBLKepling <90485361+BBLKepling@users.noreply.github.com>
2024-05-01 22:19:07 -0700

    1.5 update

--- Source/Foamalope/DeathActionWorker_FoamExplosion.cs
+++ Source/Foamalope/DeathActionWorker_FoamExplosion.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using RimWorld;
 using Verse;
+using Verse.AI.Group;
 
 namespace Foamalope
 {
@@ -11,7 +12,7 @@ namespace Foamalope
     {
         public override RulePackDef DeathRules => RulePackDefOf.Transition_DiedExplosive;
 
-        public override void PawnDied(Corpse corpse)
+        public override void PawnDied(Corpse corpse, Lord prevLord)
         {
             GenExplosion.DoExplosion(radius: (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 0) ? 1.9f : ((corpse.InnerPawn.ageTracker.CurLifeStageIndex != 1) ? 4.9f : 2.9f), center: corpse.Position, map: corpse.Map, damType: DamageDefOf.Extinguish, instigator: corpse.InnerPawn, damAmount: -1, armorPenetration: -1f, explosionSound: null, weapon: null, projectile: null, intendedTarget: null, postExplosionSpawnThingDef: ThingDefOf.Filth_FireFoam, postExplosionSpawnChance: 1f, postExplosionSpawnThingCount: (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 0) ? 1 : ((corpse.InnerPawn.ageTracker.CurLifeStageIndex != 1) ? 3 : 2));
         }
